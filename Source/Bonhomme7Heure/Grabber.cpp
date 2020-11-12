063c9fd45cacb772d001e26f143f1d3f7d19abc0
ben43 <benbru003@gmail.com>
2020-11-11 22:45:28 -0500

    Grabber code is now functionnal
    The grabber code is functionnal, only miss to extend the class and make the calls to the function when button is pressed.

---
+++ Source/Bonhomme7Heure/Grabber.cpp
@@ -0,0 +1,143 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "DrawDebugHelpers.h"
+#include "Engine/World.h"
+#include "GameFramework/PlayerController.h"
+#include "Components/InputComponent.h"
+#include "Grabber.h"
+
+// Sets default values for this component's properties
+UGrabber::UGrabber()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UGrabber::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	FindPhysicsHandle();
+	
+}
+
+//This find the physic handle component for the grabber and stores it in a variable declared in the header file.
+//This component will handle the physics of the handler
+void UGrabber::FindPhysicsHandle()
+{
+	physichandler = GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
+	if(!physichandler)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("%s does not have a physic handler, or the game will either crash or the grabber will not work."), *GetOwner()->GetName());
+	}
+}
+
+
+
+// Called every frame
+void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	//Move The object we are holding with us
+	if (!physichandler) { return; } //prevent crash when there is no physic handler
+
+	//Make the object follow the player
+	if(physichandler->GrabbedComponent)
+	{
+		physichandler->SetTargetLocation(GetPlayerReach());
+	}
+}
+
+
+void UGrabber::Grab()
+{
+
+	// ray-cast and return the body that was touched by the it.
+	FHitResult HitResult = GetFirstPhysicBodyInReach();
+
+	//Give the reference to the actor or component that was struck by the ray-cast
+	UPrimitiveComponent* ComponentToGrab = HitResult.GetComponent();
+
+	AActor* ActorHit = HitResult.GetActor();
+
+	//Get the range of the ray-cast for the handler to grab it at the good range
+	FVector LineTraceEnd = GetPlayerReach();
+
+	//Attach the actor if it is a valid actor
+	if(ActorHit)
+	{
+		if (!physichandler) { return; } //verify if there is a physichandler to avoid crash
+
+		physichandler->GrabComponentAtLocation(
+			ComponentToGrab,
+			NAME_None,
+			LineTraceEnd
+		);
+	}
+
+
+
+}
+
+void UGrabber::Release()
+{
+	physichandler->ReleaseComponent();
+}
+
+
+FHitResult UGrabber::GetFirstPhysicBodyInReach()
+{
+
+	FVector playerViewLocation;
+	FVector LineTraceEnd = GetPlayerReach();
+
+	FHitResult Hit;
+
+	//Ray-cast to the distance set by the reach variable.
+	FCollisionQueryParams TraceParams(
+	FName(TEXT("")), // This is the trace tag, not sure what it means ...
+	false, //Decide if we use complex collision
+	GetOwner() // This are the ignore actors
+	);
+
+	//Detect what we just hit
+
+	GetWorld()->LineTraceSingleByObjectType(
+		OUT Hit,				//out the object hit
+		playerViewLocation,		//Start of the line, in this case player vector
+		LineTraceEnd,			//End of the line.
+		FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),	//This get the target info to compare with the one below
+		TraceParams	//Refer to the parameters we said we want and compare them with the target hit parameter
+	);
+
+	//Store the actor we just hit someone
+	AActor* ActorHit = Hit.GetActor();
+
+	return Hit;
+
+}
+
+
+FVector UGrabber::GetPlayerReach()
+{
+	FVector playerViewLocation;
+	FRotator playerViewRotation;
+
+	//Get player location and rotation
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+		OUT playerViewLocation,
+		OUT playerViewRotation
+	);
+
+	//LineTrace end is created here, it is a point (FVector) at which the ray-cast will end
+	FVector LineTraceEnd = playerViewLocation + playerViewRotation.Vector() * Reach;
+	
+	return LineTraceEnd;
+}
---
+++ Source/Bonhomme7Heure/Grabber.h
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+#include "Components/ArrowComponent.h"
+#include "PhysicsEngine/PhysicsHandleComponent.h"
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Grabber.generated.h"
+
+
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class BONHOMME7HEURE_API UGrabber : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UGrabber();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+private:
+
+	UPROPERTY(EditAnywhere)
+		float Reach = 200.f;
+
+	UPhysicsHandleComponent* physichandler = nullptr;
+	UInputComponent* InputComponent = nullptr;
+	UArrowComponent* playerArrow = nullptr;
+
+	UFUNCTION(BlueprintCallable)
+		void Grab();
+
+	UFUNCTION(BlueprintCallable)
+		void Release();
+
+	void FindPhysicsHandle();
+	void SetupInputComponent();
+
+	FHitResult GetFirstPhysicBodyInReach();
+
+	FVector GetPlayerReach();
+
+
+};
