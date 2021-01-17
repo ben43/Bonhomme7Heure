49e68879929aeb1769df38180f12f02472ab5484
FlynnEastwood <aflynneastwood@gmail.com>
2021-01-17 01:36:02 -0500

    Added a openable door on the safe

--- Source/Bonhomme7Heure/Grabber.h
+++ Source/Bonhomme7Heure/Grabber.h
@@ -28,8 +28,8 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-	UPROPERTY(EditAnywhere)
-		AActor* ActorcHits;
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere)
+		AActor* ActorsHits;
 
 private:
 
